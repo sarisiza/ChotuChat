162beee90a22c0372953275b14c191f00b9c2a5a
sarisiza <sarita.iza@gmail.com>
2025-05-08 22:13:40 -0600

    Added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.gms.google-services") version ("4.4.1") apply false
+    id("com.google.firebase.crashlytics") version ("2.9.9")apply false
+    id("com.google.dagger.hilt.android") version ("2.49") apply false
 }
